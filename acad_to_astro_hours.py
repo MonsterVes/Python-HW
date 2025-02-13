604c0af29b05778166470e454400169063237a1a
MonsterVes <vszaprianova@gmail.com>
2025-02-13 11:09:26 +0200

    update

--- acad_to_astro_hours.py
+++ acad_to_astro_hours.py
@@ -1,10 +1,11 @@
 x = 40  # Academical Hour
 y = 60  # Astronomical Hour
-z = 15  # Break (per 3 academical hours)
+z = 20  # Break (per 3 academical hours)
 n = 64  # Course lenght in academical hours
 
-course_length_var1 = ((n*x + (n/3)*z)-z)/y
+course_length_var1 = ((n*x + (n/4)*z))/y
 print("Var1: ", course_length_var1)
 
-course_lenght_var2 = ((3*x+z)*(n/3)-z)/y
+course_lenght_var2 = ((4*x+z)*(n/4))/y
 print("Var2: ", course_lenght_var2)
+
