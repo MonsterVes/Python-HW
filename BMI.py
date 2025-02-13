f1352e6130e5dbaca1b5a8de2cb33dafd44bec17
MonsterVes <vszaprianova@gmail.com>
2025-02-13 15:50:11 +0200

    comment deletion

--- BMI.py
+++ BMI.py
@@ -1,5 +1,6 @@
-w = weight_in_kg = 57           # Weight in kilograms
-h = height_in_meters = 1.68     # Height in meters
+w = weight_in_kg = 57
+h = height_in_meters = 1.68
+
 
 # round(x,2) is showing only the first digit after the floating point, when the second one is 0
 body_mass_index = round((w/(h*h)),2)
