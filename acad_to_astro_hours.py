3677baa240be0e13d6ee41a62b0617516e34bf41
MonsterVes <vszaprianova@gmail.com>
2025-02-13 15:48:57 +0200

    comment deletion

--- acad_to_astro_hours.py
+++ acad_to_astro_hours.py
@@ -1,7 +1,7 @@
-x = academic_hour = 40                  # Academic Hour
-y = astronomical_hour = 60              # Astronomical Hour
-z = break_duration = 20                 # Break (per 3 academical hours)
-n = course_legnt_in_acad_hours = 64     # Course lenght in academical hours
+x = academic_hour = 40
+y = astronomical_hour = 60
+z = break_duration = 20         # Break (per 3 academical hours)
+n = course_legnt_in_acad_hours = 64
 
 # round(x,2) is showing only the first digit after the floating point, when the second one is 0
 course_length_astronomical_hours_var1 = (((n*x + (n/4)*z))/y)
