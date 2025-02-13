bb945230e46b86fd7325b7240701804e4829a0ad
MonsterVes <vszaprianova@gmail.com>
2025-02-13 11:27:09 +0200

    round fix

--- acad_to_astro_hours.py
+++ acad_to_astro_hours.py
@@ -3,9 +3,9 @@ y = 60  # Astronomical Hour
 z = 20  # Break (per 3 academical hours)
 n = 64  # Course lenght in academical hours
 
-course_length_var1 = ((n*x + (n/4)*z))/y
-print("Var1: ", course_length_var1)
+course_length_var1 = (((n*x + (n/4)*z))/y)
+print(f"Total duration in astronomical hours: {course_length_var1:.2f}")
 
-course_lenght_var2 = ((4*x+z)*(n/4))/y
-print("Var2: ", course_lenght_var2)
+course_length_var2 = ((4*x+z)*(n/4))/y
+print(f"Total duration in astronomical hours: {course_length_var2:.2f}")
 
