ac192ecb3fc9ed56e88adb60c8bc5f80394523a0
MonsterVes <vszaprianova@gmail.com>
2025-03-14 23:56:57 +0200

    matrix

--- lists.py
+++ lists.py
@@ -166,21 +166,22 @@
 #         [4, 5, 12, 13]
 
 # YOUR CODE HERE
-matrix_size = int(input("Enter matrix size: "))
-matrix = []
-for number in range(1, matrix_size+1):
-    element = [number]
-    matrix.append(element)  # First column/element
-    col_count = 2   # Start for second column/element
-    while col_count < matrix_size+1:
-        if col_count%2 == 0:    # If the column is even the elements should decrease
-            element.append((col_count*matrix_size + 1) - element[0])
-            col_count+=1
-        else: # If the column is odd the elements should increase
-            element.append((col_count - 1) * matrix_size + element[0] )
-            col_count+=1
-for element in matrix:
-    print(element)  
+# matrix_size = int(input("Enter matrix size: "))
+
+# matrix = []
+# for number in range(1, matrix_size+1):
+#     element = [number]
+#     matrix.append(element)  # First column/element
+#     col_count = 2   # Start for second column/element
+#     while col_count < matrix_size+1:
+#         if col_count%2 == 0:    # If the column is even the elements should decrease
+#             element.append((col_count*matrix_size + 1) - element[0])
+#             col_count+=1
+#         else: # If the column is odd the elements should increase
+#             element.append((col_count - 1) * matrix_size + element[0] )
+#             col_count+=1
+# for element in matrix:
+#     print(element)  
 
 
 
